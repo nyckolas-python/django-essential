f394f439347864bf750d113486f9684d460ec244
nyckolas <nyckolas.work@gmail.com>
2023-01-06 15:29:51 +0200

    feature: add 3 endpoint: index, signup, all_users

--- app/urls.py
+++ app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from proxy_user.views import Index, CreateUser, AllUsers
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', Index.as_view(), name="index"),
+    path('signup/', CreateUser.as_view(), name="signup"),
+    path('all_users/', AllUsers.as_view(), name="all_users")
 ]
--- proxy_user/views.py
+++ proxy_user/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import TemplateView
+from django.views.generic import FormView
+from .models import Person
 
-# Create your views here.
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+
+class CreateUser(FormView):
+    form_class = UserCreationForm
+    template_name = 'signup.html'
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return super(CreateUser, self).form_valid(form)
+
+
+class AllUsers(TemplateView):
+    template_name = "all_users.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['users'] = Person.people.get_staff_users()
+        return context
