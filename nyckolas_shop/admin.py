31ad435d7d068f4826fe10fd3437f2714e84e6b2
nyckolas <nyckolas.python@gmail.com>
2023-03-10 16:36:20 +0200

    chore: register models for nyckolas_shop

--- nyckolas_shop/admin.py
+++ nyckolas_shop/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import TV, Notebook, Dishwasher, Brand, Category, VacuumCleaner, Item, Promo
 
-# Register your models here.
+
+for model in [TV, Notebook, Dishwasher, Brand, Category, VacuumCleaner, Promo]:
+    admin.site.register(model)
