6a17b08317d296545353a297d178176f6a005f78
nyckolas <nyckolas.python@gmail.com>
2023-03-12 20:15:19 +0200

    feature: add views, templates

--- jinja_app/views.py
+++ jinja_app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def jinja_test(request):
+    return render(request, 'index_django.html',
+                  context={'user_data': {'name': 'First'},
+                           'user_info': {'key1': 'value1',
+                                         'key2': 'value2'}},
+                  using='my_backend')
+
+
+def django_test(request):
+    return render(request, 'index_django.html',
+                  context={'user_data': [{'name': 'First'},
+                                         {'name': 'Second'}]})
