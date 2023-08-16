0d308914bae49a47c5f9c84057867e4aecd19b19
nyckolas <nyckolas.python@gmail.com>
2023-08-16 21:37:16 +0300

    fix: resolve conflicts user_proxy with newest graph_app

--- user_proxy/models.py
+++ user_proxy/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
 from user_proxy.managers import PersonManager
 
 
@@ -11,11 +11,16 @@ class Person(User):
         Use to change the behavior of the model.
     """
     people = PersonManager()
+    person_groups = models.ManyToManyField(Group, related_name='people')
+    person_permissions = models.ManyToManyField(
+        Permission, related_name='people'
+    )
 
     class Meta:
         # It is possible to hide unnecessary fields and change the sorting
         proxy = True
         ordering = ('first_name',)
+        swappable = 'AUTH_USER_MODEL'
     # It is possible to add custom methods
     def do_something(self):
         print(self.username)
